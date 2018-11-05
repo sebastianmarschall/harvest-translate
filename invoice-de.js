a6a2298a6f8adde8698a9d0b792fa9eedd873006
Sebastian Marschall <office@sebastianmarschall.com>
2018-11-05 15:15:44 +0100

    Create invoice-de.js

---
+++ invoice-de.js
@@ -0,0 +1,6 @@
+javascript:( function() {
+    document.getElementById('profile_t_invoice').value = 'Rechnung';
+    document.getElementById('profile_t_from').value = 'Von';
+    document.getElementById('profile_t_for').value = 'Für';
+    document.getElementById('profile_t_id').value = 'Rechnungs Nr.:';
+})();
