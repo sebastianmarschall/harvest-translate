5393856e3a823774947a30b84e47d94d071e57f9
Sebastian Marschall <office@sebastianmarschall.com>
2018-11-05 15:47:04 +0100

    Create estimate-de.js

---
+++ estimate-de.js
@@ -0,0 +1,22 @@
+javascript:( function() {
+    document.getElementById('profile_t_estimate').value = 'Rechnung';
+    document.getElementById('profile_t_from').value = 'Von';
+    document.getElementById('profile_t_for').value = 'Für';
+    document.getElementById('profile_t_id').value = 'Rechnungs Nr.:';
+    document.getElementById('profile_t_po_number').value = '';
+    document.getElementById('profile_t_issue_date').value = 'Datum';
+    document.getElementById('profile_t_tax').value = 'USt.';
+    document.getElementById('profile_t_tax2').value = '';
+    document.getElementById('profile_t_discount').value = 'Rabatt';
+    document.getElementById('profile_t_subject').value = '';
+    document.getElementById('profile_t_type').value = '';
+    document.getElementById('profile_t_description').value = 'Leistung';
+    document.getElementById('profile_t_quantity').value = 'Stunden';
+    document.getElementById('profile_t_unit_price').value = 'Stundensatz';
+    document.getElementById('profile_t_amount').value = 'Betrag';
+    document.getElementById('profile_t_subtotal').value = 'Summe exkl. USt.';
+    document.getElementById('profile_t_amount_due').value = 'Gesamtbetrag';
+    document.getElementById('profile_t_notes').value = '';
+    document.getElementById('profile_t_page_numbers').value = '';
+    document.getElementById('profile_t_file_attachments').value = '';
+})();
